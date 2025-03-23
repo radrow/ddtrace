f3641eb9a0c0d9af17d8f3e3bd2a6f6183765deb
radrow <radrowicki@gmail.com>
2025-03-23 18:36:42 +0100

    bench script

---
+++ bench.sh
@@ -0,0 +1,58 @@
+make
+
+SEED=2137
+TIMESTAMP=$(date +%d-%m-%Y--%T)
+
+append_meta() {
+    OUT_FILE=$1
+    TEST_FILE=$2
+    PDELAY=$3
+    TIMEOUT=$4
+
+    METADATA="# TEST FILE=$(md5sum ${TEST_FILE}); PROBE DELAY=${PDELAY}; TIMEOUT=${TIMEOUT}; SEED=${SEED}"
+    # echo $METADATA >> $OUT_FILE
+}
+
+eval_echo() {
+    echo "$*"
+    "$@"
+}
+
+run_bench() {
+    TESTFILE=$1
+    TESTTYPE=$2
+    PDELAY=$3
+    TIMEOUT=$4
+
+    [[ $TESTTYPE != bc || $TESTTYPE != ts ]] && echo "Bad test type" && return 1
+
+    # Backup name
+    OUT_FILE="${TESTTYPE}_${TIMESTAMP}.csv"
+    OUT_FILE_ARG=$(if [[ $TESTTYPE = ts ]]; then echo -n "csv"; elif [[ $TESTTYPE = bc ]]; then echo -n "stats-csv"; else return 1; fi)
+
+    eval_echo ./dlstalk\
+              "${TESTFILE}"\
+              "--seed=${SEED}"\
+              --trace-proc\
+              --silent\
+              $(if [[ $PDELAY = "unmonitored" ]]; then echo -n --unmonitored; else echo -n "--probe-delay=$PDELAY"; fi)\
+              "--${OUT_FILE_ARG}=${OUT_FILE}"\
+              "--timeout=$TIMEOUT"
+
+    append_meta $OUT_FILE $TESTFILE $PDELAY $TIMEOUT
+
+    # Save under deterministic name
+    DETNAME="${TESTTYPE}_${PDELAY}.csv"
+    eval_echo cp $OUT_FILE $DETNAME
+}
+
+run_bench scenarios/big.conf ts -1 20000
+run_bench scenarios/big.conf ts 500 20000
+run_bench scenarios/big.conf ts 5000 20000
+
+run_bench scenarios/bench.conf bc unmonitored 1000
+run_bench scenarios/bench.conf bc -1 10000
+run_bench scenarios/bench.conf bc 1000 10000
+run_bench scenarios/bench.conf bc 5000 20000
+
+# export PDELAY=5000 && make && ./dlstalk scenarios/bench.conf --seed=2137 --trace-proc "--probe-delay=$PDELAY" "--stats-csv=run_$(date +%d-%m-%Y--%T)_probe-delay_$PDELAY.csv" --silent --timeout=20000
